3ff672ad697a0d401eddb4b69fe0c70c4cdc2bc5
DanielAlegriaLino <daniel.alegria.lino@gmail.com>
2023-01-25 19:33:28 -0600

    Renamed function fetchGraph to fetchApi

--- code/Script.js
+++ code/Script.js
@@ -5,13 +5,13 @@ let path_promise
 let domain = "https://dijkstraapi.azurewebsites.net/api/dijkstrahttp"
 let getgraph_parameters = '?code=PNYvobtS0QSQr-n7akHmfv-ZYqcbf0k08mGjSfjC3H3nAzFuadZrUw%3D%3D&getgraph=true&none='
 
-const fetchGraph= async (url,params) => {
+const fetchApi= async (url,params) => {
     return await fetch(url+params)
     .then((res) => res.json())
   }
 
 
-graph_promise = fetchGraph(domain.concat(getgraph_parameters))
+graph_promise = fetchApi(domain.concat(getgraph_parameters))
 
 const svg = document.getElementById("d3svg")
 const button_calculate_route = document.getElementById("button_calculate_route")
@@ -21,7 +21,7 @@ button_calculate_route.addEventListener("click", () =>{
   let start = document.getElementById("start").value.toLowerCase();
   let goal = document.getElementById("goal").value.toLowerCase();
   let getpath_parameters =  `?code=PNYvobtS0QSQr-n7akHmfv-ZYqcbf0k08mGjSfjC3H3nAzFuadZrUw%3D%3D&start=${start}&goal=${goal}&none=`
-  path_promise = fetchGraph(domain+getpath_parameters)
+  path_promise = fetchApi(domain+getpath_parameters)
   console.log(path_promise)
   graph_promise.then((graph) => {
     path_promise.then((path) =>{
