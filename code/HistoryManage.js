db48b20cd2bf95ad28c47ef918d9af91eabaff9b
DanielAlegriaLino <daniel.alegria.lino@gmail.com>
2023-01-25 23:01:51 -0600

    added history system

--- code/HistoryManage.js
+++ code/HistoryManage.js
@@ -1,17 +1,55 @@
-export const saveRecord = (start,goal,complexity, ) => {
-    let history = getRecords();
-    if(history.length > 4){
-        history.shift()
-    }    
-    history.push({ 
-        'start':start,
-        'goal':goal,
-        'complexity':complexity
-    })
-    localStorage.setItem('history', history)
+const max_history_length = 5
+
+export const saveRecord = (start,goal,complexity) => {
+    for(let i = 0; i<max_history_length; i++){
+        if(localStorage.getItem(`history${i}`) == undefined){
+            localStorage.setItem(`history${i}`,[start,goal,complexity,Date.now()])
+            return true
+        }
+    }
+    for(let i = max_history_length-1; i>0; i--){
+        localStorage.setItem(`history${i}`,localStorage.getItem(`history${i-1}`))
+    }
+    let currentTime = `${(new Date()).getHours()}:${(new Date()).getMinutes()}:${(new Date()).getSeconds()}`
+    localStorage.setItem(`history0`,[start,goal,complexity,currentTime])
 }
 
-export const getRecords = () => {
-    localStorage.getItem("history")
+export const updateHtmlTable = (TableId) => {
+    const table = document.getElementById(TableId)
+    table.innerHTML=`<div class="table-row table-head">
+    <div class="table-cell">
+        <p>Origen</p>
+    </div>
+    <div class="table-cell">
+        <p>Destino</p>
+    </div>
+    <div class="table-cell before-cell">
+        <p>Complejidad</p>
+    </div>
+    <div class="table-cell last-cell">
+        <p>Fecha y Hora</p>
+    </div>
+</div>`
+    for(let i = 0; i<max_history_length; i++){
+        if(localStorage.getItem(`history${i}`) == undefined){
+            return true
+        }
+        let record = localStorage.getItem((`history${i}`)).split(",")
+        console.log(record[1])
+        table.innerHTML+=`        <div class="table-row">
+        <div class="table-cell">
+            <p>${record[0]}</p>
+        </div>
+        <div class="table-cell">
+            <p>${record[1]}</p>
+        </div>
+        <div class="table-cell before-cell">
+            <p>${record[2]}</p>
+        </div>
+        <div class="table-cell last-cell">
+            <p>${record[3]}</p>
+        </div>
+    </div>`
+    }
 }
 
--- code/Script.js
+++ code/Script.js
@@ -1,5 +1,5 @@
 import { drawGraph } from "./DrawingSVG.js";
-import {  } from "./HistoryManage.js";
+import { saveRecord,updateHtmlTable } from "./HistoryManage.js";
 
 const domain = "https://dijkstraapi.azurewebsites.net/api/dijkstrahttp"
 let getgraph_parameters = '?code=PNYvobtS0QSQr-n7akHmfv-ZYqcbf0k08mGjSfjC3H3nAzFuadZrUw%3D%3D&getgraph=true&none='
@@ -16,6 +16,7 @@ const button_calculate_route = document.getElementById("button_calculate_route")
 const history = document.getElementById('table-box')
 
 window.addEventListener("load", () => {
+  updateHtmlTable("table-box")
   graph_promise = fetchApi(domain.concat(getgraph_parameters))
   graph_promise.then((graph) => {
     svg.innerHTML=''
@@ -27,13 +28,19 @@ button_calculate_route.addEventListener("click", () =>{
   let start = document.getElementById("start").value.toLowerCase();
   let goal = document.getElementById("goal").value.toLowerCase();
   let getpath_parameters =  `?code=PNYvobtS0QSQr-n7akHmfv-ZYqcbf0k08mGjSfjC3H3nAzFuadZrUw%3D%3D&start=${start}&goal=${goal}&none=`
+  let getcomplexity_parameters = `?code=PNYvobtS0QSQr-n7akHmfv-ZYqcbf0k08mGjSfjC3H3nAzFuadZrUw%3D%3D&start=${start}&goal=${goal}&getdistance=true&uwu=`
   let path_promise = fetchApi(domain+getpath_parameters)
+  let complexity_promise = fetchApi(domain+getcomplexity_parameters)
   graph_promise.then((graph) => {
     path_promise.then((path) =>{
-      svg.innerHTML=''
-      drawGraph(graph,path)
+      complexity_promise.then((complexity)=>{
+        saveRecord(start, goal,complexity)
+        svg.innerHTML=''
+        drawGraph(graph,path)
+      })
     })
   })
+  updateHtmlTable("table-box")
 })
 
 
