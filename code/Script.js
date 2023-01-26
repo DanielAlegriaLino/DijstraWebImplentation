27b72dd58a7c53ce069924b334de9736df6a3144
DanielAlegriaLino <daniel.alegria.lino@gmail.com>
2023-01-25 20:07:57 -0600

    reordered code for separate events and accions

--- code/Script.js
+++ code/Script.js
@@ -1,29 +1,33 @@
 import { drawGraph } from "./DrawingSVG.js";
 import {  } from "./HistoryManage.js";
 
-let graph_promise;
-let path_promise
-let domain = "https://dijkstraapi.azurewebsites.net/api/dijkstrahttp"
+const domain = "https://dijkstraapi.azurewebsites.net/api/dijkstrahttp"
 let getgraph_parameters = '?code=PNYvobtS0QSQr-n7akHmfv-ZYqcbf0k08mGjSfjC3H3nAzFuadZrUw%3D%3D&getgraph=true&none='
 
+let graph_promise;
+
 const fetchApi= async (url,params) => {
-    return await fetch(url+params)
-    .then((res) => res.json())
-  }
-
-
-graph_promise = fetchApi(domain.concat(getgraph_parameters))
+  return await fetch(url+params)
+  .then((res) => res.json())
+}
 
 const svg = document.getElementById("d3svg")
 const button_calculate_route = document.getElementById("button_calculate_route")
+const history = document.getElementById('table-box')
 
+window.addEventListener("load", () => {
+  graph_promise = fetchApi(domain.concat(getgraph_parameters))
+  graph_promise.then((graph) => {
+    svg.innerHTML=''
+    drawGraph(graph)
+  })
+})
 
 button_calculate_route.addEventListener("click", () =>{
   let start = document.getElementById("start").value.toLowerCase();
   let goal = document.getElementById("goal").value.toLowerCase();
   let getpath_parameters =  `?code=PNYvobtS0QSQr-n7akHmfv-ZYqcbf0k08mGjSfjC3H3nAzFuadZrUw%3D%3D&start=${start}&goal=${goal}&none=`
-  path_promise = fetchApi(domain+getpath_parameters)
-  console.log(path_promise)
+  let path_promise = fetchApi(domain+getpath_parameters)
   graph_promise.then((graph) => {
     path_promise.then((path) =>{
       svg.innerHTML=''
@@ -32,11 +36,7 @@ button_calculate_route.addEventListener("click", () =>{
   })
 })
 
-graph_promise.then((graph) => {
-  svg.innerHTML=''
-  drawGraph(graph)
-})
 
-const history = document.getElementById('table-box')
+
 
 
